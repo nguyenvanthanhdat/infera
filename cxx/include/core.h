ac9ea995cb33ef69621f187d6868559d147fba71
hieupth <64821726+hieupth@users.noreply.github.com>
2024-05-30 19:55:35 +0700

    chore: add model header at core.h

---
+++ cxx/include/core.h
@@ -0,0 +1,51 @@
+#ifndef __CRT_CORE_H__
+#define __CRT_CORE_H__
+
+#include<string>
+#include <#include <onnxruntime_cxx_api.h>
+
+namespace cinrt::model
+{
+  class Model
+  {
+  protected:
+    std::shared_ptr<ORT::Env> _env;           
+    std::shared_ptr<ORT::Allocator> _allocator;
+    std::unique_ptr<ORT::Session> _session;
+    std::unique_ptr<ORT::SessionOptions> _sessionOptions;
+
+  public: 
+    Model(
+      std::string model, 
+      bool parallel = true, 
+      int graphOpLevel = 0, 
+      int interThreads = 0, 
+      int intraThreads = 0,
+      std::<vector<std::string>> providers = nullptr
+    );
+
+  protected: 
+    Model(
+      std::shared_ptr<ORT::Env> env, 
+      std::shared_ptr<ORT::Allocator> allocator, 
+      std::string model,
+      bool parallel = true,
+      int graphOpLevel = 0,
+      int interThreads = 0,
+      int intraThreads = 0,
+      std::<vector<std::string>> providers = nullptr
+    );
+
+    std::unique_ptr<Ort::SessionOptions> getSessionOptions(
+      bool parallel = true,
+      int graphOpLevel = 0,
+      int interThreads = 0,
+      int intraThreads = 0
+    );
+
+  public:
+    std::shared_ptr<Ort::Value> run(const Ort::Value& inputs);
+  };
+};
+
+#endif
