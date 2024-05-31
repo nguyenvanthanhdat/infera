8d31856e05132d051d0645b68425dd26ad237129
Hung Nguyen <hungnq131002@gmail.com>
2024-05-31 09:42:22 +0700

    feat: add script test for 2 models

---
+++ scripts/yolov7-test.cpp
@@ -0,0 +1,191 @@
+#define UNICODE
+
+#include <onnxruntime_cxx_api.h>
+#include <iostream>
+#include <unistd.h>
+#include <chrono>
+#include <opencv2/opencv.hpp>
+#include <utils/image.cpp>
+
+#include "ORTManager.h"
+#include <benchmark/benchmark.h>
+
+void Run_session(ORTManager &ortManager, Ort::Value &inputTensor, Ort::Value &outputTensor1, std::string sessionName){ 
+     // inter + intra, test from ORTSesstionInterface.h
+    ortManager.run(sessionName, inputTensor, outputTensor1); 
+}
+
+
+static void BM_Run_Function(benchmark::State& state) {
+    // This code gets timed
+    using std::chrono::steady_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::duration;
+    using std::chrono::milliseconds;
+
+    Ort::Env env;
+    Ort::RunOptions runOptions;
+    Ort::Session session(nullptr);
+
+    constexpr int64_t numChannels = 3;
+    // constexpr int64_t width = 256;
+    // constexpr int64_t height = 256;
+    // change to 640x640
+    constexpr int64_t width = 640;
+    constexpr int64_t height = 640;
+    constexpr int64_t numPixels = width * height;
+    constexpr int64_t numElements = numChannels * numPixels;
+
+    // print present path
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        // std::cout << "Current working dir: " << cwd << std::endl;
+    } else {
+        //printf("getcwd() error");
+    }
+    const std::string imageFileD = "../tests/sample/8D5U5524_D.png"; 
+    const std::string imageFileS = "../tests/sample/8D5U5524_S.png";
+    const std::string imageFileT = "../tests/sample/8D5U5524_T.png";
+    const std::string outputImage = "../tests/sample/output_1.png";
+    const auto modelPath = "../models/yolov7-headface-v1.onnx";
+    
+    // load images
+    cv::Mat imageD = loadImage(imageFileD);
+    cv::Mat imageS = loadImage(imageFileS);
+    cv::Mat imageT = loadImage(imageFileT);
+
+    // notificate the user if the image is not loaded
+    if (imageD.empty() || imageS.empty() || imageT.empty()) {
+        // std::cout << "Image not loaded.\n";
+    }
+
+    int originalWidth = imageD.cols;
+    int originalHeight = imageD.rows;
+
+    // resize image
+    cv::Mat imaged, images, imaget; 
+    cv::resize(imageD, imaged, cv::Size(width, height));
+    cv::resize(imageS, images, cv::Size(width, height));
+    cv::resize(imageT, imaget, cv::Size(width, height));
+
+    imageD = fixedImage(imageD);
+    imageS = fixedImage(imageS);
+    imageT = fixedImage(imageT);
+    // std::cout << "Image shape: " << imageD.size() << std::endl;
+ 
+    // std::cout << std::string(50, '-') << std::endl;
+    // std::cout << "Convert image to vec";
+    std::vector<std::vector<std::vector<float>>> imagedVec = mat2vec(imaged); //List[List[List[float]]]
+    std::vector<std::vector<std::vector<float>>> imagesVec = mat2vec(images);
+    std::vector<std::vector<std::vector<float>>> imagetVec = mat2vec(imaget);
+    std::vector<std::vector<std::vector<float>>> imageDVec = mat2vec(imageD);
+    std::vector<std::vector<std::vector<float>>> imageSVec = mat2vec(imageS);
+    std::vector<std::vector<std::vector<float>>> imageTVec = mat2vec(imageT);
+    // std::cout << "Image shape: " << imagedVec.size() << "x" << imagedVec[0].size() << "x" <<  imagedVec[0][0].size() << std::endl;
+
+    // transpose image (channels, newHeight, newWidth) -> (newHeight, newWidth, channels)
+    // std::vector<int> af_shape = {1, 2, 0};
+    // std::vector<std::vector<std::vector<float>>> transposeImageD = transposeImage(
+    //     imageDVec, af_shape);
+    // std::vector<std::vector<std::vector<float>>> transposeImageS = transposeImage(
+    //     imageSVec, af_shape);
+    // std::vector<std::vector<std::vector<float>>> transposeImageT = transposeImage(
+    //     imageTVec, af_shape);
+    // std::cout << "Transposed image shape: " << transposeImageD.size() << "x" << transposeImageD[0].size() << "x" <<  transposeImageD[0][0].size() << std::endl;
+    
+    // concatenate images shape (3, newHeight, newWidth) -> (3 * 3, newHeight, newWidth)
+    std::vector<std::vector<std::vector<float>>> inputMatrix;
+    // inputMatrix.insert(inputMatrix.end(), transposeImageD.begin(), transposeImageD.end());
+    // inputMatrix.insert(inputMatrix.end(), transposeImageS.begin(), transposeImageS.end());
+    // inputMatrix.insert(inputMatrix.end(), transposeImageT.begin(), transposeImageT.end());
+    
+    inputMatrix.insert(inputMatrix.end(), imagedVec.begin(), imagedVec.end());
+    inputMatrix.insert(inputMatrix.end(), imagesVec.begin(), imagesVec.end());
+    inputMatrix.insert(inputMatrix.end(), imagetVec.begin(), imagetVec.end());
+    // std::cout << std::string(50, '-') << std::endl;
+    // std::cout << "Concatenate image shape: " << inputMatrix.size() << "x" << inputMatrix[0].size() << "x" <<  inputMatrix[0][0].size() << std::endl;
+
+    // define shape
+    // const std::array<int64_t, 4> inputShape = {1, 9, height, width};
+    const std::array<int64_t, 4> inputShape = {3, 3, height, width};
+    // const std::array<int64_t, 4> outputShape = {1, 3, height, width};
+    const std::array<int64_t, 2> outputShape1 = {0, 7};
+    const std::array<int64_t, 2> outputShape2 = {0, 22};
+
+    // define array with new shape
+    // std::array<float, static_cast<std::size_t>(newHeight * newWidth * 3 * 3)> input;
+    // std::array<float, newHeight * newWidth> output; // output shape is [batch_size,3,256,256]
+    std::vector<float> input(height * width * 3 * 3); // *vectors -> flatten list[list[list[float]]] -> list[float]
+    // std::vector<float> output(height * width * 3);
+    std::vector<float> output1(7);
+    // std::vector<float> output2(22);
+
+    size_t index = 0;
+    for (int c = 0; c < 3 * 3; ++c) {
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                input[index] = inputMatrix[c][i][j];
+                index++;
+            }
+        }
+    }
+
+    Ort::MemoryInfo memory_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
+    
+    Ort::Value inputTensor = Ort::Value::CreateTensor<float>(
+        memory_info, input.data(), input.size(), inputShape.data(), inputShape.size()
+    );
+    // benchmark::RegisterBenchmark(str::string "Input", BM_test, inputTensor)
+
+    // Ort::Value outputTensor1 = Ort::Value::CreateTensor<float>(
+    //     memory_info, output.data(), output.size(), outputShape.data(), outputShape.size()
+    // );
+    Ort::Value outputTensor1 = Ort::Value::CreateTensor<float>(
+        memory_info, output1.data(), output1.size(), outputShape1.data(), outputShape1.size()
+    );
+    // benchmark::RegisterBenchmark(std::string "Output", BM_test, output1)
+
+    //
+    // Ort::Value outputTensor2 = Ort::Value::CreateTensor<float>(
+    //     memory_info, output2.data(), output2.size(), outputShape2.data(), outputShape2.size()
+    // );
+    
+    ORTManager ortManager;
+    std::string sessionName = "test";
+    std::string threadOption = "inter_intra";
+    std::string execution = "sequential";
+    std::string mode = "async";
+    std::string optimize_path = "";
+
+    ortManager.createSession(
+        sessionName, 
+        modelPath, 
+        threadOption,
+        state.range(0), //optmize_level
+        optimize_path, 
+        mode, 
+        execution, 
+        state.range(1) //num_thread
+    );
+    // Perform setup here
+    for (auto _ : state) {
+        Run_session(
+            ortManager,
+            inputTensor,
+            outputTensor1,
+            sessionName
+        );
+        
+    }
+}
+
+
+// Register the function as a benchmark
+BENCHMARK(BM_Run_Function)->ArgsProduct({
+    { 0, 1, 2, 3},
+    { 0, 1, 2, 3, 4 }
+})->Unit(
+    benchmark::kMillisecond
+);
+// Run the benchmark
+BENCHMARK_MAIN();
