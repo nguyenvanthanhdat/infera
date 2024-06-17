ac1c3b6fc6dc67a8c5da7ee9f7ceb4b8563e5432
hieupth <64821726+hieupth@users.noreply.github.com>
2024-06-17 15:08:38 +0700

    chore: test cmake build

--- tests/hello.cpp
+++ tests/hello.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <onnxruntime_cxx_api.h>
-#include <core.h>
 
 int main(){
-    std::cout << "Hello, from Cinnamon Runtime!\n";
+    std::cout << "Hello, from Cinnamon Runtime!\nAvailable Providers:" << std::endl;
+    auto providers = Ort::GetAvailableProviders();
+    for (std::string p : providers) {
+        std::cout << "- " << p << std::endl;
+    };
     return 0;
 }
